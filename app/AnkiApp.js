9401b411aafea4e89256b75417e898bc2730af09
John Pham <hprobotic@gmail.com>
2017-06-30 15:41:50 +0700

    [f] add auth page

--- app/AnkiApp.js
+++ app/AnkiApp.js
@@ -1,14 +1,14 @@
 //import liraries
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, StatusBar} from 'react-native';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import { AuthView } from './views';
 
 // create a component
 class AnkiApp extends Component {
   render() {
-    console.log('go here');
     if(!this.props.isLoggedIn) {
-      return <LoginScreen />;
+      return <AuthView />;
     }
     return (
       <View style={styles.container}>
